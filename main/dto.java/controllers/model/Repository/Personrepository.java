5b9f8dc8f75e2bb42c80f54d84ccb3aac2ec41d7
Gayatri <bobburi.gayatri@dxc.com>
2022-02-02 03:39:00 -0800

    spring-boot

---
+++ main/dto.java/controllers/model/Repository/Personrepository.java
@@ -0,0 +1,8 @@
+package com.dxc.personrestapi.repository;
+import com.dxc.personrestapi.model.Person;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface PersonRepository  extends JpaRepository<Person,Integer>{
+    
+}
+© 2022 Git
